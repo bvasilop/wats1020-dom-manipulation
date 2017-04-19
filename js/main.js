db6e1f975af247ec91a480086ad152294259999d
bvasilop <bvasilop@gmail.com>
2017-04-19 15:56:42 -0700

    Added on click event listener

--- js/main.js
+++ js/main.js
@@ -34,6 +34,21 @@ $( document ).ready(function() {
     //      4. Change the text of the "view details" button to read "hide details" so the user
     //          understands they can hide the text again.
 
+    $('.view-details').on('click', function(event) {
+        console.log(event);
+        var targetElement = event.target;
+        var container = targetElement.parentElement.parentElement;
+        $(container).find('.details').each(function( index, el){
+        if ($(el) .is ( ':visible')) {
+            $(el) .fadeOut();
+            targetElement.innerText = "View Details"
+        } else {
+              $(el).fadeIn();
+              targetElement.innerText= "Hide Details"
+        }
+    });
+  })
+
     // TODO: Create a function that listens for clicks on the voting buttons and
     // looks at the `data-vote` attribute on each button to see what was voted for,
     // then determines the updated vote breakdown to adjust the progress bars.
